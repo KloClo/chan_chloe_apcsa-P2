25ab0526a6f5c87d725369b3b4eafcd508ad7d39
Chloe Chan <icefiresparrow@gmail.com>
2019-02-04 09:49:33 -0800

    Added Unit02 and completed TrapArea lab

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,17 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double areaCalculated = 0;
+		
+		areaCalculated = (0.5 * (base1 + base2) * height);
+		
+		return areaCalculated;
+	}
+}
