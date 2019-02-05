1101c007210dee9b6f1c1f1a8ae71f808b5ce0dc
Chloe Chan <icefiresparrow@gmail.com>
2019-02-05 10:08:11 -0800

    Updated AddSubMult.java

--- Unit04/src/AddSubMult.java
+++ Unit04/src/AddSubMult.java
@@ -9,6 +9,18 @@ public class AddSubMult
 {
 	public static double check( double a, double b )
 	{
-		return 0;
+		double result = 0;
+		
+		if (a > b) {
+			result = a - b;
+		}
+		else if (a < b) {
+			result = b - a;
+		}
+		else {
+			result = a*b;
+		}
+		
+		return result;
 	}
 }
