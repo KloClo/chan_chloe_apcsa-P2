c188494a8938c4f5b660b94462a49e2e49cf8b89
Chloe Chan <icefiresparrow@gmail.com>
2019-02-06 11:56:06 -0800

    Updated WordsCompare.java

--- Unit05/src/WordsCompareRunner.java
+++ Unit05/src/WordsCompareRunner.java
@@ -8,11 +8,24 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class WordsCompareRunner
 {
    public static void main( String args[] )
    {
 		//add test cases
+	   Scanner keyboard = new Scanner(in);
+	   
+	   out.print("Enter the first word: ");
+	   String firstWord = keyboard.nextLine();
+	   
+	   out.print("Enter the second word: ");
+	   String secondWord = keyboard.nextLine();
 
+	   WordsCompare test = new WordsCompare (firstWord, secondWord);
+	   test.compare();
+	   out.println(test.toString());
+	   
 	}
 }
