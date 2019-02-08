ca7748983e637802b16ab663b297e09e4adc4152
Chloe Chan <icefiresparrow@gmail.com>
2019-02-08 10:48:29 -0800

    Updated Word.java and WordRunner.java

--- Unit06/src/WordRunner.java
+++ Unit06/src/WordRunner.java
@@ -4,10 +4,23 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class WordRunner
 {
 	public static void main ( String[] args )
 	{
 		//add test cases		
+		Scanner keyboard = new Scanner(in);
+		
+		out.print("What would would you like backwards? Enter: ");
+		String word = keyboard.nextLine();
+		
+		Word test = new Word(word);
+		test.getFirstChar();
+		test.getLastChar();
+		test.getBackWards();
+		out.println(test.toString());
+		
 	}
 }
