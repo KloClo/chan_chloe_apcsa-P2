95e0468e2df53b4f04855328f96a6e098c05bbfd
Chloe Chan <icefiresparrow@gmail.com>
2019-02-08 10:32:23 -0800

    Updated TriangleOne.java and TriangleOneRunner.java

--- Unit06/src/TriangleOneRunner.java
+++ Unit06/src/TriangleOneRunner.java
@@ -4,10 +4,20 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriangleOneRunner
 {
 	public static void main ( String[] args )
 	{
 		//add test cases		
+		Scanner keyboard = new Scanner(in);
+		
+		out.print("Give me a word: ");
+		String word = keyboard.nextLine();
+		
+		TriangleOne test = new TriangleOne(word);
+		test.print();
+		
 	}
 }
