ef0fc98d048f264ea4156951e52dc8a6015bab53
Chloe Chan <icefiresparrow@gmail.com>
2019-02-11 10:16:33 -0800

    Updated Perfect.java and PerfectRunner.java

--- Unit06/src/Perfect.java
+++ Unit06/src/Perfect.java
@@ -9,17 +9,45 @@ public class Perfect
    private int number;
 
 	//add constructors
-
+   public Perfect() {
+	   number = 0;
+   }
+   
+   public Perfect(int num) {
+	   setNumber(num);
+   }
 	//add a set method
+   
+   public void setNumber(int num) {
+	   number = num;
+   }
 
 	public boolean isPerfect()
 	{
+		//loop to find all divisors, adding each divisor to sum
+		int sum = 0;
+		
+		for (int x = 1; x < number; x++) {
+			if (number%x == 0) {
+				sum = sum + x;
+			}
+		}
+		
+		//if sum equals num return true
+		if (sum == number) {
+			return true;
+		}
+
 		return false;
 	}
 
 	//add a toString	
 	public String toString() {
-		return "";
+		if (isPerfect()) {
+			return number + " is perfect";
+		}
+		
+		return number + " is not perfect";
 		
 	}
 }
