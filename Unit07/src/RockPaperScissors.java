a84be32bbed46a01f67b8778d1af90a15ded184a
Chloe Chan <icefiresparrow@gmail.com>
2019-02-14 11:12:22 -0800

    Started RSP.java activity

--- Unit07/src/RockPaperScissors.java
+++ Unit07/src/RockPaperScissors.java
@@ -12,19 +12,45 @@ public class RockPaperScissors
 
 	public RockPaperScissors()
 	{
+		playChoice = "";
+		compChoice = "";
 	}
 
 	public RockPaperScissors(String player)
 	{
+		setPlayers(player);
 	}
 
 	public void setPlayers(String player)
 	{
+		playChoice = player.toUpperCase();
+		
+		int num = (int)(Math.random()*3);
+		
+		if (num == 0) {
+			compChoice = "R";
+		}
+		else if (num == 1) {
+			compChoice = "P";
+		}
+		else if (num == 2) {
+			compChoice = "S";
+		}
 	}
 
 	public String determineWinner()
 	{
 		String winner="";
+		
+		if (playChoice.equals(compChoice)) {
+			winner = "draw";
+		}
+		else if (playChoice.equals("R") && compChoice.equals("S")) {
+			
+		}
+		
+		//give all cases above
+		
 		return winner;
 	}
 
