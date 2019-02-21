923f4a1ae8c835b70ec069e5de7d3be958d5ac3f
Chloe Chan <icefiresparrow@gmail.com>
2019-02-21 10:29:45 -0800

    Updated TriangleFive.java

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -34,6 +34,49 @@ public class TriangleFive
 	public String toString()
 	{
 		String output="";
+		char currentLetter = letter;
+		int letterNum = amount;
+		int subtractColumn = 0;
+		int subtractRow = 0;
+		
+		int rowNum = amount;
+		
+
+		for (int row = amount; row > 0; row--)
+		{
+			
+			for (int column = rowNum; column > 0; column--)
+			{
+				while (letterNum != 0)
+				{
+					output = output + currentLetter;
+					letterNum--;
+				}
+				
+				output = output + " ";
+				if (currentLetter == 90) 
+				{
+					currentLetter = 65;
+				}
+				else
+				{
+					currentLetter = (char)(currentLetter + 1);
+				}
+				
+				subtractColumn++;
+				letterNum = amount - subtractColumn;
+
+			}
+			
+			currentLetter = letter;
+			letterNum = amount;
+			subtractColumn = 0;
+			subtractRow++;
+			rowNum = amount - subtractRow;
+			
+			output = output + "\n";
+		}
+		
 		return output;
 	}
 }
