284d831e0da44d26514c85e1dc8599ac5b7b5d46
Chloe Chan <icefiresparrow@gmail.com>
2019-03-05 11:12:09 -0800

    Finished ListDown activity

--- Unit09/src/DownRunner.java
+++ Unit09/src/DownRunner.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -6,6 +8,17 @@
 public class DownRunner
 {
 	public static void main( String args[] )
-	{											
+	{
+		ListDown s = new ListDown();
+		int[] array = new int[] {10,9,8,7,6,5,4,3,2,1,-99};
+		
+		ArrayList<Integer> vals = new ArrayList<Integer>();
+		
+		for (int x = 0; x < array.length; x++)
+		{
+			vals.add(array[x]);
+		}
+
+		System.out.println(s.go(vals));
 	}
 }
--- Unit09/src/ListDown.java
+++ Unit09/src/ListDown.java
@@ -12,6 +12,16 @@ public class ListDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(List<Integer> numArray)
 	{
+		int num = numArray.get(0);
+		
+		for (int x = 1; x < numArray.size(); x++)
+		{
+			if (num <= numArray.get(x))
+			{
+				return false;
+			}
+		}
+		
 		return true;
 	}	
 }
